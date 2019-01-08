2b8fb5063eee5892baad5f180cb675609a1cf81a
nafsan <iamnafsan@gmail.com>
2019-01-08 17:38:22 +0600

    Android Project

--- app/src/main/java/com/example/nafsan/virtualdiary/MainActivity.java
+++ app/src/main/java/com/example/nafsan/virtualdiary/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.nafsan.virtualdiary;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+    private EditText editText1,editText2;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText1=findViewById(R.id.emailId);
+        editText2=findViewById(R.id.passwordId);
     }
 }
