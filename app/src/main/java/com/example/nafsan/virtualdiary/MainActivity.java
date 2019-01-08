5c74289e401f4b7f332ac6bdf67c430e50c8e6c8
nafsan <iamnafsan@gmail.com>
2019-01-08 17:49:19 +0600

    Android Project

--- app/src/main/java/com/example/nafsan/virtualdiary/MainActivity.java
+++ app/src/main/java/com/example/nafsan/virtualdiary/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.nafsan.virtualdiary;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -17,5 +19,16 @@ public class MainActivity extends Activity {
         editText1=findViewById(R.id.emailId);
         editText2=findViewById(R.id.passwordId);
         button=findViewById(R.id.buttonId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editText1.getText().toString()==editText2.getText().toString())
+                {
+                    Toast.makeText(MainActivity.this,"Success",Toast.LENGTH_SHORT).show();
+                }
+                
+            }
+        });
     }
 }
